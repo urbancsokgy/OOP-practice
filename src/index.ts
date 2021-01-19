0f7d91960f3deb1fe63465f45e428a64be359715
urbancsokgy <urbancsokgy@gmail.com>
2021-01-19 20:32:01 +0100

    change origin OK

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,42 @@
 import './style.scss';
 
-console.log(`Program has been inited.`);
+
+class Vehicle{
+    name: string;
+    /*van be waater, groun, air */
+    type: string;
+    
+    constructor(name: string){
+        
+        this.name=name;
+    }
+    move(from: number, to: number): string{
+        return `This vehicle is moving from ${from} to ${to}`
+    }
+
+
+}
+
+class GroundVehicle extends Vehicle{
+    type: string="ground";
+    wheels: number=0;
+    constructor (name: string, wheels: number){
+        super(name); 
+        this.wheels=wheels;
+    }
+}
+
+class Lorry extends GroundVehicle{
+    capacity: number;
+    constructor(name: string, wheels: number, capacity: number){
+        super(name, wheels);
+        this.capacity=capacity;
+    }
+}
+
+
+let truck=new Vehicle("ize");
+let lorry=new Lorry("ize", 3, 4);
+console.log(truck.move(1, 2 ));
+console.table(lorry);
+
